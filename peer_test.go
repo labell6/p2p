a8ca9f6b4738e369bcb30cdf5d4835c39af2e38e
root <root@miningt80.finclusion.com>
2017-05-16 05:47:46 -0500

    update wrapper

--- peer_test.go
+++ peer_test.go
@@ -1,19 +1,16 @@
-// Implementation of Tests for peer.go
+//Implementation of a simple peer to peer chat application using
+//the net/http package for communication.
 
 package main
 
 import (
-  "os/exec"
-  "testing"
-  "log"
-  "strings"
+	"net/http"
+	"reflect"
+	"testing"
+        "os/exec"
+        "log"
+        "strings"
 )
-
-
-type TestMsg struct {
-        Message string
-        From    Peer
-}
 
 type TestPeer struct {
         Name    string
@@ -61,9 +58,320 @@ func TestData(t *testing.T) {
         log.Println("server detected: ",&testpeercontroller)
 }
 
+func TestNewPeerData(t *testing.T) {
+	type args struct {
+		self Peer
+	}
+	tests := []struct {
+		name string
+		args args
+		want *PeerData
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewPeerData(tt.args.self); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewPeerData() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
+func TestPeerData_start(t *testing.T) {
+	type fields struct {
+		Self            Peer
+		Peers           Peers
+		addPeers        chan (Peer)
+		exitPeer        chan (Peer)
+		currentPeers    chan (Peers)
+		getCurrentPeers chan (bool)
+		receivedMsg     chan (Msg)
+		userMsg         chan (Msg)
+	}
+	tests := []struct {
+		name   string
+		fields fields
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			peercontroller := &PeerData{
+				Self:            tt.fields.Self,
+				Peers:           tt.fields.Peers,
+				addPeers:        tt.fields.addPeers,
+				exitPeer:        tt.fields.exitPeer,
+				currentPeers:    tt.fields.currentPeers,
+				getCurrentPeers: tt.fields.getCurrentPeers,
+				receivedMsg:     tt.fields.receivedMsg,
+				userMsg:         tt.fields.userMsg,
+			}
+			peercontroller.start()
+		})
+	}
+}
 
+func TestPeerData_cmdLoop(t *testing.T) {
+	type fields struct {
+		Self            Peer
+		Peers           Peers
+		addPeers        chan (Peer)
+		exitPeer        chan (Peer)
+		currentPeers    chan (Peers)
+		getCurrentPeers chan (bool)
+		receivedMsg     chan (Msg)
+		userMsg         chan (Msg)
+	}
+	tests := []struct {
+		name   string
+		fields fields
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			peercontroller := &PeerData{
+				Self:            tt.fields.Self,
+				Peers:           tt.fields.Peers,
+				addPeers:        tt.fields.addPeers,
+				exitPeer:        tt.fields.exitPeer,
+				currentPeers:    tt.fields.currentPeers,
+				getCurrentPeers: tt.fields.getCurrentPeers,
+				receivedMsg:     tt.fields.receivedMsg,
+				userMsg:         tt.fields.userMsg,
+			}
+			peercontroller.cmdLoop()
+		})
+	}
+}
 
+func TestPeerData_existingPeer(t *testing.T) {
+	type fields struct {
+		Self            Peer
+		Peers           Peers
+		addPeers        chan (Peer)
+		exitPeer        chan (Peer)
+		currentPeers    chan (Peers)
+		getCurrentPeers chan (bool)
+		receivedMsg     chan (Msg)
+		userMsg         chan (Msg)
+	}
+	type args struct {
+		peer Peer
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   bool
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			peercontroller := &PeerData{
+				Self:            tt.fields.Self,
+				Peers:           tt.fields.Peers,
+				addPeers:        tt.fields.addPeers,
+				exitPeer:        tt.fields.exitPeer,
+				currentPeers:    tt.fields.currentPeers,
+				getCurrentPeers: tt.fields.getCurrentPeers,
+				receivedMsg:     tt.fields.receivedMsg,
+				userMsg:         tt.fields.userMsg,
+			}
+			if got := peercontroller.existingPeer(tt.args.peer); got != tt.want {
+				t.Errorf("PeerData.existingPeer() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
+func TestPeerData_sendAdd(t *testing.T) {
+	type fields struct {
+		Self            Peer
+		Peers           Peers
+		addPeers        chan (Peer)
+		exitPeer        chan (Peer)
+		currentPeers    chan (Peers)
+		getCurrentPeers chan (bool)
+		receivedMsg     chan (Msg)
+		userMsg         chan (Msg)
+	}
+	type args struct {
+		peer Peer
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			peercontroller := &PeerData{
+				Self:            tt.fields.Self,
+				Peers:           tt.fields.Peers,
+				addPeers:        tt.fields.addPeers,
+				exitPeer:        tt.fields.exitPeer,
+				currentPeers:    tt.fields.currentPeers,
+				getCurrentPeers: tt.fields.getCurrentPeers,
+				receivedMsg:     tt.fields.receivedMsg,
+				userMsg:         tt.fields.userMsg,
+			}
+			peercontroller.sendAdd(tt.args.peer)
+		})
+	}
+}
 
-     
+func TestPeerData_sendTx(t *testing.T) {
+	type fields struct {
+		Self            Peer
+		Peers           Peers
+		addPeers        chan (Peer)
+		exitPeer        chan (Peer)
+		currentPeers    chan (Peers)
+		getCurrentPeers chan (bool)
+		receivedMsg     chan (Msg)
+		userMsg         chan (Msg)
+	}
+	type args struct {
+		peer Peer
+		msg  Msg
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			peercontroller := &PeerData{
+				Self:            tt.fields.Self,
+				Peers:           tt.fields.Peers,
+				addPeers:        tt.fields.addPeers,
+				exitPeer:        tt.fields.exitPeer,
+				currentPeers:    tt.fields.currentPeers,
+				getCurrentPeers: tt.fields.getCurrentPeers,
+				receivedMsg:     tt.fields.receivedMsg,
+				userMsg:         tt.fields.userMsg,
+			}
+			peercontroller.sendTx(tt.args.peer, tt.args.msg)
+		})
+	}
+}
+
+func TestPeerData_webListener(t *testing.T) {
+	type fields struct {
+		Self            Peer
+		Peers           Peers
+		addPeers        chan (Peer)
+		exitPeer        chan (Peer)
+		currentPeers    chan (Peers)
+		getCurrentPeers chan (bool)
+		receivedMsg     chan (Msg)
+		userMsg         chan (Msg)
+	}
+	tests := []struct {
+		name   string
+		fields fields
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			peercontroller := &PeerData{
+				Self:            tt.fields.Self,
+				Peers:           tt.fields.Peers,
+				addPeers:        tt.fields.addPeers,
+				exitPeer:        tt.fields.exitPeer,
+				currentPeers:    tt.fields.currentPeers,
+				getCurrentPeers: tt.fields.getCurrentPeers,
+				receivedMsg:     tt.fields.receivedMsg,
+				userMsg:         tt.fields.userMsg,
+			}
+			peercontroller.webListener()
+		})
+	}
+}
+
+func Test_createAddHandler(t *testing.T) {
+	type args struct {
+		peercontroller *PeerData
+	}
+	tests := []struct {
+		name string
+		args args
+		want func(http.ResponseWriter, *http.Request)
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := createAddHandler(tt.args.peercontroller); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("createAddHandler() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_createTxHandler(t *testing.T) {
+	type args struct {
+		peercontroller *PeerData
+	}
+	tests := []struct {
+		name string
+		args args
+		want func(http.ResponseWriter, *http.Request)
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := createTxHandler(tt.args.peercontroller); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("createTxHandler() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPeerData_startStdinListener(t *testing.T) {
+	type fields struct {
+		Self            Peer
+		Peers           Peers
+		addPeers        chan (Peer)
+		exitPeer        chan (Peer)
+		currentPeers    chan (Peers)
+		getCurrentPeers chan (bool)
+		receivedMsg     chan (Msg)
+		userMsg         chan (Msg)
+	}
+	type args struct {
+		sender Peer
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			peercontroller := &PeerData{
+				Self:            tt.fields.Self,
+				Peers:           tt.fields.Peers,
+				addPeers:        tt.fields.addPeers,
+				exitPeer:        tt.fields.exitPeer,
+				currentPeers:    tt.fields.currentPeers,
+				getCurrentPeers: tt.fields.getCurrentPeers,
+				receivedMsg:     tt.fields.receivedMsg,
+				userMsg:         tt.fields.userMsg,
+			}
+			peercontroller.startStdinListener(tt.args.sender)
+		})
+	}
+}
